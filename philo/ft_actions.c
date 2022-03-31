edbeaca4d4a074520d40824a34b41a85b4d48763
Vladimir NAFISSI <vnafissi@e2r5p18.clusters.42paris.fr>
2022-03-31 17:24:23 +0200

    pb eating regle

--- philo/ft_actions.c
+++ philo/ft_actions.c
@@ -6,7 +6,7 @@
 /*   By: vnafissi <vnafissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 18:30:35 by vnafissi          #+#    #+#             */
-/*   Updated: 2022/03/31 15:29:53 by vnafissi         ###   ########.fr       */
+/*   Updated: 2022/03/31 17:15:31 by vnafissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ int	ft_start_eating(t_philo *philo, int first_fork, int second_fork)
 	pthread_mutex_lock(&philo->game->forks[second_fork]);
 	ft_printf(philo, "has taken a fork");
 	ft_printf(philo, "is eating");
-	ft_usleep(philo->game->time_to_eat, philo);
 	pthread_mutex_lock(&philo->lock_philo);
 	philo->nb_times_eat++;
 	philo->last_meal = ft_gettimeofday_ms();
 	pthread_mutex_unlock(&philo->lock_philo);
+	ft_usleep(philo->game->time_to_eat, philo);
 	pthread_mutex_unlock(&philo->game->forks[second_fork]);
 	pthread_mutex_unlock(&philo->game->forks[first_fork]);
 	return (0);
